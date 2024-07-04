655adfd5aab037fd21ad8b385aef7a82d56d7c9b
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-07-04 15:23:52 -0300

    corrigindo products pages

--- components/customCard/index.jsx
+++ components/customCard/index.jsx
@@ -4,7 +4,6 @@ import { Card, CardHeader, CardBody } from "@nextui-org/react";
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import CustomModal from '../CustomModal';
-import NextImage from "next/image";
 import CustomSkeleton from './../skeleton/index';
 
 export default function CustomCard({ imagem, titulo, descricao, tamanho, referencia, descButton, classe, id, modalTitle,loading }) {
@@ -18,10 +17,10 @@ export default function CustomCard({ imagem, titulo, descricao, tamanho, referen
     }
   };
 
-
   if (loading) {
     return <CustomSkeleton />; 
   }
+
   return (
     <Card className="py-4">
       <CardHeader className="pb-2 pt-2 px-4 flex-col items-start">
@@ -42,7 +41,6 @@ export default function CustomCard({ imagem, titulo, descricao, tamanho, referen
           <Card className=" dark:bg-zinc-800">
             <CardBody className="overflow-visible ">
               <Image
-                as={NextImage}
                 alt="Card background"
                 className="w-full object-cover rounded-xl"
                 src={imagem}
