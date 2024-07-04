e58943cb6b5d1c536477c25e7048ad67383d3335
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-07-04 18:51:33 -0300

    sjuste cardCustom

--- components/customCard/index.jsx
+++ components/customCard/index.jsx
@@ -31,23 +31,24 @@ export default function CustomCard({ imagem, titulo, descricao, tamanho, referen
           
           <Card className="py-4 dark:bg-zinc-800">
             <CardHeader className="pb-2 pt-2 px-4 flex-col items-start">
-              <div className='flex flex-col gap-2'>
-              <p className="font-bold text-large">Nome: {titulo}</p>
-              <p className="text-tiny  font-bold">Descrição: {descricao}</p>
-              <p className="text-tiny  font-bold">tamanho: {descricao}</p>
-              <p className="text-tiny  font-bold">nº de referencia: {referencia}</p>
-              <small className="text-default-500">Código: 5468461681861</small>
-              </div>
-    
-            </CardHeader>
-            <CardBody className="overflow-visible py-2">
-              <Image
+            <Image
                 alt="Card background"
                 className="w-full object-cover rounded-xl  "
                 src={imagem}
                 width={270}
                 height={270}
               />
+             
+    
+            </CardHeader>
+            <CardBody className="overflow-visible py-2 px-5">
+            <div className='flex flex-col gap-2'>
+              <p className="font-bold text-large">Nome: {titulo}</p>
+              <p className="text-tiny  font-bold">Descrição: {descricao}</p>
+              <p className="text-tiny  font-bold">tamanho: {descricao}</p>
+              <p className="text-tiny  font-bold">nº de referencia: {referencia}</p>
+              <small className="text-default-500">Código: 5468461681861</small>
+              </div>
             </CardBody>
           </Card>
         </CustomModal>
