b8e5ffd3427814dbc0e4a9197259c1a5f90ef827
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-07-05 18:53:34 -0300

    corrigindo api

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000/categorias';
+const baseUrl = 'https://radani-api.vercel.app/categorias';
 
 export async function getAllCategories() {
   const response = await axios.get(baseUrl);
