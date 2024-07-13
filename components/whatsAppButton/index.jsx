e3566bd2c2dbddc7fac868f93c6cf869b6684a6b
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-07-13 14:22:56 -0300

    whatsButton

--- components/whatsAppButton/index.jsx
+++ components/whatsAppButton/index.jsx
@@ -4,9 +4,9 @@ import { ImWhatsapp } from "react-icons/im";
 export default function WhatsAppButton() {
   return (
     <div className=''>
-      <div className='rounded-full   bg-green-500 motion-safe:animate-bounce " focus:animate-none hover:animate-none  text-md  p-3  tracking-wide '>        
+      <div className='rounded-full   bg-green-500 motion-safe:animate-bounce " focus:animate-none hover:animate-none  text-md  p-2  tracking-wide '>        
         <a href=" https://wa.me/" target='_blank'>
-        <ImWhatsapp size={20} />
+        <ImWhatsapp size={25} />
         </a>
       </div>
     </div>
