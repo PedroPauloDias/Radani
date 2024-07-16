7bdb2b73af83c595ee555a938365455d5eac1ff0
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-07-16 11:55:21 -0300

    mensagem whats

--- components/whatsAppButton/index.jsx
+++ components/whatsAppButton/index.jsx
@@ -5,7 +5,7 @@ export default function WhatsAppButton() {
   return (
     <div className=''>
       <div className='rounded-full   relative bg-green-500 motion-safe:animate-bounce " focus:animate-none hover:animate-none  text-md  p-2  tracking-wide '>        
-        <a href=" https://wa.me/" target='_blank'>
+        <a href=" https://wa.me/55015997303095?text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es!" target='_blank'>
         <ImWhatsapp size={25} />
         </a>
       </div>
